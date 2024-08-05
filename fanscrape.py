69567a449b46aa2e3ef38dc3f13a6359f8d7c654
Jakan <github@jakan.co>
2024-08-05 14:41:30 -0400

    Change searching for network index
    If there are multiple times that the network name is in the path select the last instance as the index, instead of the first

--- fanscrape.py
+++ fanscrape.py
@@ -522,9 +522,11 @@ def get_path_info(path):
     network = 'Fansly' if 'Fansly' in str(path) else 'OnlyFans'
     try:
         path_parts = [item.lower() for item in path.parts]
-        index = path_parts.index(network.lower())
-        if (path.parts[index + 1] == "sites"):
-            index = path_parts.index(network.lower(), index + 1)
+        """
+        If the network is in the path multiple times get the last one
+        """
+        indexes = [index for index, element in enumerate(path_parts) if element == network]
+        index = indexes[-1] if indexes else None
         if index + 1 < len(path.parts):
             return  path.parts[index + 1], network, Path(*path.parts[:index + 2])
         raise ValueError
